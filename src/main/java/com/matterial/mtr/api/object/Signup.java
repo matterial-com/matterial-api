9d5052c0719169f227954dcb655b15005ea89485
Sascha Sadikni <sascha.sadikni@matterial.com>
2017-05-18 17:39:02 +0200

    InstanceHandling
    InstanceHandling - #72 - Signup

---
+++ src/main/java/com/matterial/mtr/api/object/Signup.java
@@ -0,0 +1,47 @@
+package com.matterial.mtr.api.object;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * <strong>Signup</strong>
+ */
+@XmlRootElement
+public class Signup implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String dataSourceDisplayName;
+    private String dataSourceReference;
+    private Licence licence;
+    
+    public Signup() {
+        // *** do nothing;
+    }
+    
+    public String getDataSourceDisplayName() {
+        return dataSourceDisplayName;
+    }
+    
+    public void setDataSourceDisplayName(String dataSourceDisplayName) {
+        this.dataSourceDisplayName = dataSourceDisplayName;
+    }
+    
+    public String getDataSourceReference() {
+        return dataSourceReference;
+    }
+    
+    public void setDataSourceReference(String dataSourceReference) {
+        this.dataSourceReference = dataSourceReference;
+    }
+    
+    public Licence getLicence() {
+        return licence;
+    }
+    
+    public void setLicence(Licence licence) {
+        this.licence = licence;
+    }
+    
+}
