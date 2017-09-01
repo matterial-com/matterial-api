a65ddd9776e23b734913a9929f526ec679cd9f0c
Sascha Sadikni <sascha.sadikni@matterial.com>
2017-09-01 17:44:37 +0200

    Notification
    Notification - #146 - WebSocket

--- src/main/java/com/matterial/mtr/api/object/Notification.java
+++ src/main/java/com/matterial/mtr/api/object/Notification.java
@@ -2,14 +2,58 @@ package com.matterial.mtr.api.object;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * <strong>Notification</strong>
  */
+@XmlRootElement
 public class Notification implements Serializable {
     
     private static final long serialVersionUID = 1L;
-
+    
+    private long instanceId;
+    private long clientId;
+    private long documentId;
     private String message;
+    
+    public Notification() {
+        // *** do nothing;
+    }
+    
+    public Notification(long instanceId, 
+                        long clientId, 
+                        long documentId, 
+                        String message) {
+        this.instanceId = instanceId;
+        this.clientId = clientId;
+        this.documentId = documentId;
+        this.message = message;
+    }
+
+    public long getInstanceId() {
+        return instanceId;
+    }
+    
+    public void setInstanceId(long instanceId) {
+        this.instanceId = instanceId;
+    }
+    
+    public long getClientId() {
+        return clientId;
+    }
+    
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+    
+    public long getDocumentId() {
+        return documentId;
+    }
+    
+    public void setDocumentId(long documentId) {
+        this.documentId = documentId;
+    }
     
     public String getMessage() {
         return message;
