c12ce8fc9f7f9a46766d6fe458fb87b526d79c26
Sascha Sadikni <sascha.sadikni@matterial.com>
2018-01-04 17:14:19 +0100

    LoginData
    Sending websocket-notification on logindata-reload

--- src/main/java/com/matterial/mtr/api/object/DocumentNotification.java
+++ src/main/java/com/matterial/mtr/api/object/DocumentNotification.java
@@ -7,27 +7,27 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class DocumentNotification extends Notification {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private long documentId;
-    
+
     public DocumentNotification() {
         // *** do nothing;
     }
-    
+
     public DocumentNotification(String message,
                                 long documentId) {
         super(message);
         this.documentId = documentId;
     }
-    
+
     public long getDocumentId() {
         return documentId;
     }
-    
+
     public void setDocumentId(long documentId) {
         this.documentId = documentId;
     }
-    
+
 }
