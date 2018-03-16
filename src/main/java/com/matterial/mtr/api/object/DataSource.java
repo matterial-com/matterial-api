d8f622dfe3915251540dae97a29b6c212d41b97f
Sascha Sadikni <sascha.sadikni@matterial.com>
2018-03-16 17:21:51 +0100

    MultiServer
    Instance handling with multi-server #193

--- src/main/java/com/matterial/mtr/api/object/DataSource.java
+++ src/main/java/com/matterial/mtr/api/object/DataSource.java
@@ -9,26 +9,27 @@ import com.matterial.mtr.api.object.meta.Identifiable;
  */
 @XmlRootElement
 public class DataSource extends ListResultEntry implements Identifiable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private long id;
     private Long activeUntilInSeconds;
     private String name;
     private String displayName;
     private String reference;
-    
+    private String server;
+
     public DataSource() {
         // *** do nothing;
     }
-    
+
     public DataSource(String name) {
         this.activeUntilInSeconds = null;
         this.name = name;
         this.displayName = name;
     }
-    
-    public DataSource(long id, Long activeUntilInSeconds, String name, String displayName, String reference) {
+
+    public DataSource(long id, Long activeUntilInSeconds, String name, String displayName, String reference, String server) {
         this(name);
         this.id = id;
         this.activeUntilInSeconds  = activeUntilInSeconds;
@@ -38,13 +39,16 @@ public class DataSource extends ListResultEntry implements Identifiable {
         if(reference != null) {
             this.reference = reference;
         }
+        if(server != null) {
+            this.server = server;
+        }
     }
-    
+
     @Override
     public long getId() {
         return id;
     }
-    
+
     @Override
     public void setId(long id) {
         this.id = id;
@@ -57,31 +61,39 @@ public class DataSource extends ListResultEntry implements Identifiable {
     public void setActiveUntilInSeconds(Long activeUntilInSeconds) {
         this.activeUntilInSeconds = activeUntilInSeconds;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getDisplayName() {
         return displayName;
     }
-    
+
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
-    
+
     public String getReference() {
         return reference;
     }
-    
+
     public void setReference(String reference) {
         this.reference = reference;
     }
-    
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
     @Override
     public String toString() {
         StringBuilder buffer = new StringBuilder();
@@ -95,6 +107,8 @@ public class DataSource extends ListResultEntry implements Identifiable {
         buffer.append(this.getDisplayName());
         buffer.append(", reference:");
         buffer.append(this.getReference());
+        buffer.append(", server:");
+        buffer.append(this.getServer());
         return buffer.toString();
     }
 
