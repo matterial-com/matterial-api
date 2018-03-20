e844c571f1c107189b523955d5bea39a4bb46c56
Sascha Sadikni <sascha.sadikni@matterial.com>
2018-03-20 14:27:12 +0100

    CommunicationData
    CommunicationData `Other` added

--- src/main/java/com/matterial/mtr/api/object/meta/EntityType.java
+++ src/main/java/com/matterial/mtr/api/object/meta/EntityType.java
@@ -17,7 +17,8 @@ public class EntityType {
     public static final long COMMUNICATION_DATA_PHONE = 14;
     public static final long COMMUNICATION_DATA_FAX = 15;
     public static final long COMMUNICATION_DATA_SKYPE = 53;
-    
+    public static final long COMMUNICATION_DATA_OTHER = 105;
+
     public static final long ROLE_FUNCTIONAL = 21;
     public static final long ROLE_CONTENT = 22;
     public static final long ROLE_PERSONAL = 24;
@@ -25,9 +26,9 @@ public class EntityType {
     public static final long ROLE_CLIENT_GATE = 55;
 
     public static final long DOCUMENT_LANGUAGE_VERSION = 48;
-    
+
     public static final long ACCOUNT = 33;
-    
+
     public static final long CONTACT_IMAGE = 102;
     public static final long ATTACHMENT = 103;
     public static final long CATEGORY = 104;
