c3c05b2824ab1ae387d7777efadc0f5f58af3d9b
Sascha Sadikni <sascha.sadikni@matterial.com>
2018-06-19 17:06:33 +0200

    Document
    Lock documents by user and sessionId #721

--- src/main/java/com/matterial/mtr/api/object/DocumentLock.java
+++ src/main/java/com/matterial/mtr/api/object/DocumentLock.java
@@ -9,19 +9,20 @@ import com.matterial.mtr.api.object.meta.Identifiable;
  */
 @XmlRootElement
 public class DocumentLock implements Identifiable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private long id;
     private Person person;
     private long documentId;
+    private String sessionId;
     private boolean lockedForMe;
     private long lockTimestampInSeconds;
-    
+
     public DocumentLock() {
         // *** do nothing;
     }
-    
+
     @Override
     public long getId() {
         return id;
@@ -31,35 +32,43 @@ public class DocumentLock implements Identifiable {
     public void setId(long id) {
         this.id = id;
     }
-    
+
     public Person getPerson() {
         return person;
     }
-    
+
     public void setPerson(Person person) {
         this.person = person;
     }
-    
+
     public long getDocumentId() {
         return documentId;
     }
-    
+
     public void setDocumentId(long documentId) {
         this.documentId = documentId;
     }
-    
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
     public boolean isLockedForMe() {
         return lockedForMe;
     }
-    
+
     public void setLockedForMe(boolean lockedForMe) {
         this.lockedForMe = lockedForMe;
     }
-    
+
     public long getLockTimestampInSeconds() {
         return lockTimestampInSeconds;
     }
-    
+
     public void setLockTimestampInSeconds(long lockTimestampInSeconds) {
         this.lockTimestampInSeconds = lockTimestampInSeconds;
     }
